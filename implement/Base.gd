ae8dfa1688d7d7b595fc337a1a259afa3a9aa787
DreamSea <amegafuru@gmail.com>
2018-05-15 23:45:30 -0500

    5x5 grid test all trees

--- implement/Base.gd
+++ implement/Base.gd
@@ -4,12 +4,20 @@ extends Control
 # var a = 2
 # var b = "textvar"
 
-func _ready():
-	var sprite = Sprite.new()
-	var texture = load("res://CraftingTactics_Object_Tilesheet.png")
-	sprite.texture = texture;
-	add_child(sprite)
 
+func _ready():
+	var slot_preload = preload("res://SlotUI.tscn")
+	
+	var grid = $Grid
+	for x in range(5):
+		for y in range(5):
+			var slot_ui = slot_preload.instance()
+			slot_ui.rect_position = Vector2(x*64, y*64)
+			slot_ui.get_node("Click").connect("pressed", self, "_select_slot", [slot_ui])
+			grid.add_child(slot_ui)
+
+func _select_slot(slot):
+	print(slot)
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
