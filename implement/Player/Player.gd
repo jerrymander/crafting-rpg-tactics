6ff89996c425156f57b6173c0a22cc7aa1ef65a8
Jerry Yuan <d.jerryuan@gmail.com>
2018-06-03 23:08:33 -0500

    Character Update

--- implement/Player/Player.gd
+++ implement/Player/Player.gd
@@ -1,13 +1,26 @@
 extends Node
 
+var max_hp = 8
+var max_ap = 12
+
 var hp = 8
 var ap = 12
+
+var crafting_levels = {}
+
+var level = 1
+
+var skill_levels = {}
 
 func description():
 	return "You!"
 
 func hp_add(amount):
 	hp += amount
+	if hp > max_hp:
+		hp = max_hp
 
 func ap_add(amount):
 	ap += amount
+	if ap > max_ap:
+		ap = max_ap
